3599bbe46020a262e6db72bb1d5ead9f4e2be1d2
AlexGrigorenco <alpsnhandy@gmail.com>
2023-05-07 18:43:37 +0300

    added menu nav, menu mobile

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 
+$(function(){
 
-document.querySelector('.menu-icon-wrapper').onclick = function(){
-    document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
-}
+    $('.menu-nav-item .link a').click(function(e){
+        $('.menu-nav-item').removeClass('active');
+        $(e.target).closest('.menu-nav-item').addClass('active');        
+    })
+    
+    $('.menu-mobile .menu-nav-item  a').click(function(e){
+        toggleMenuMobile()     
+    })
+
+    function toggleMenuMobile(){
+        $('.menu-icon').toggleClass('menu-icon-active');
+        $('.menu-mobile-wrapper').toggleClass('active');
+        $('body').toggleClass('noscroll');
+    }
+
+    $('.menu-icon-wrapper').click(function() {
+        toggleMenuMobile()
+      });
+      
+})
