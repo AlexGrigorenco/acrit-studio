aa203c53fabf4fcc47477b570a8a945207f809d0
AlexGrigorenco <alpsnhandy@gmail.com>
2023-05-08 01:25:58 +0300

    added header content

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ $(function(){
         $(e.target).closest('.menu-nav-item').addClass('active');        
     })
     
-    $('.menu-mobile .menu-nav-item  a').click(function(e){
+    $('.menu-mobile .menu-nav-item .sub-menu a').click(function(e){
         toggleMenuMobile()     
     })
 
     function toggleMenuMobile(){
         $('.menu-icon').toggleClass('menu-icon-active');
-        $('.menu-mobile-wrapper').toggleClass('active');
+        $('.menu-mobile-overlay').toggleClass('active');
         $('body').toggleClass('noscroll');
     }
 
